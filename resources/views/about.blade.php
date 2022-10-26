7dc53947dd105e9a81a6733e6f4ffb76c20b51ab
Yusuf-wahyu <yusufwahyusyahroni@gmail.com>
2022-10-26 15:47:19 +0700

    menyelesaikan video ke 4 baru

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>WPU Blog | About</title>
-</head>
-<body>
-    <h1> halaman About</h1>
-    <h3> <?= $name; ?> </h3>
-    <p> <?= $email; ?> </p>
-    <img src="img/<?= $image; ?>" alt="$name" width="200">
+@extends ('layouts.main')
 
-</body>
-</html>
+@section('container')
+    <h1> halaman About</h1>
+    <h3> {{ $name }} </h3>
+    <p> {{ $email }} </p>
+    <img src="img/{{ $image }}" alt="{{ $name }}" width="200">
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,73 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view ('home');
+    return view ('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view ('about', [
+        "title" => "About",
         "name" => "Yusuf Wahyu Syahroni",
         "email" => "yuswhys@gmail.com",
         "image" => "yusuf.jpg"
     ]);
 });
 
+
+
 Route::get('/blog', function () {
-    return view ('posts');
+    $blog_posts =[
+        [
+            "title" => "judul post pertama",
+            "slug" =>"judul-post-pertama",
+            "author" => "Cupz Stuword",
+            "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde, suscipit? Facere nisi non ipsam exercitationem, est consequuntur illum impedit a delectus sint necessitatibus iste sit vel saepe quidem ullam nostrum."
+        ],
+        [
+            "title" => "judul post kedua",
+            "slug" =>"judul-post-kedua",
+            "author" => "Yusuf Wahyu Syahroni",
+            "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde, suscipit? Facere nisi non ipsam exercitationem, est consequuntur illum impedit a delectus sint necessitatibus iste sit vel saepe quidem ullam nostrum."
+        ],
+    ];
+
+    
+    return view ('posts', [
+        "title"=> "Posts",
+        "posts"=> $blog_posts
+    ]);
+});
+
+
+
+// halaman single post
+Route::get('posts/{slug}', function($slug) {
+    $blog_posts =[
+        [
+            "title" => "judul post pertama",
+            "slug" =>"judul-post-pertama",
+            "author" => "Cupz Stuword",
+            "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde, suscipit? Facere nisi non ipsam exercitationem, est consequuntur illum impedit a delectus sint necessitatibus iste sit vel saepe quidem ullam nostrum."
+        ],
+        [
+            "title" => "judul post kedua",
+            "slug" =>"judul-post-kedua",
+            "author" => "Yusuf Wahyu Syahroni",
+            "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde, suscipit? Facere nisi non ipsam exercitationem, est consequuntur illum impedit a delectus sint necessitatibus iste sit vel saepe quidem ullam nostrum."
+        ],
+    ];
+
+    $new_post = [];
+    foreach ($blog_posts as $post){
+        if($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
