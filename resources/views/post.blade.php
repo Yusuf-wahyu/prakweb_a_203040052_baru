423f382aedfb114e20a5c2515008c6528234dd92
Rifaldi0007 <rifaldia513@gmail.com>
2022-10-31 15:45:43 +0700

    menyelesaikan video 12

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,10 +2,19 @@
 
 @section('container')
 
-    <h1 class="mb-5">{{ $post->title }}</h1>
-    <p>By. <a href="/authors/{{ $post->author->username }}" class=text-decoration-none>{{ $post->author->name }}</a> <a href="/categories/{{ $post->category->slug }} "class=text-decoration-none> {{ $post->category->name   }}</a></p>
 
-{!! $post->body !!}
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <h1 class="mb-5">{{ $post->title }}</h1>
+                <p>By. <a href="/authors/{{ $post->author->username }}" class=text-decoration-none>{{ $post->author->name }}</a> <a href="/categories/{{ $post->category->slug }} "class=text-decoration-none> {{ $post->category->name   }}</a></p>
+                
+                    {!! $post->body !!}
+                
+                <a href="/posts" c;ass="d-block mt-3">Back to posts</a>
+        </div>
+    </div>
+</div>
+ 
 
-<a href="/posts" c;ass="d-block mt-3">Back to posts</a>
 @endsection
